4660f568cae3d85150dca3872a239c028519c103
Emily Dokken <dokkenemily@gmail.com>
2022-03-16 22:18:17 -0700

    add thougth schema and reaction count virtual

--- models/Thought.js
+++ models/Thought.js
@@ -1,18 +1,33 @@
-const { Schema, model, Types } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const ThoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            min: [1, "Why so few thoughts?"],
-            max: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        }
-    }
-)
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min: [1, "Why so few thoughts?"],
+      max: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJson: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+})
