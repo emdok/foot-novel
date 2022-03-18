61bec624f97cf6475da2993b0fdc8181e506b084
Emily Dokken <dokkenemily@gmail.com>
2022-03-17 23:49:29 -0700

    remove populate from getAllThoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -46,10 +46,6 @@ const thoughtController = {
   // get all thoughts
   getAllThoughts(req, res) {
     Thought.find({})
-    .populate({
-      path: 'thoughts',
-      select: '-__v'
-    })
     .select('-__v')
     .sort({ _id: -1 })
     .then(dbUserData => res.json(dbUserData))
