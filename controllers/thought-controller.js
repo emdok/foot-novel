6c542ab70076785a7d79cd3578589d14ff4306bc
Emily Dokken <dokkenemily@gmail.com>
2022-03-17 23:10:04 -0700

    add update a single thought and remove a single thoguth

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -61,6 +61,33 @@ const thoughtController = {
 
   // update a single thought
   updateThought({ params, body }, res) {
-    
+    Thought.findOneAndUpdate(
+      { _id: params.id },
+      body,
+      { new: true, runValidators: true }
+    )
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
+
+  // remove a thought by ID
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No thought found with this id!' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
   }
-}
+};
+
+module.exports = thoughtController;
