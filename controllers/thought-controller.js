114f65b43d07a981be082ac1a3ef8c5dcb8ca037
Emily Dokken <dokkenemily@gmail.com>
2022-03-17 23:31:41 -0700

    test

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -6,6 +6,7 @@ const thoughtController = {
     console.log(body);
     Thought.create(body)
     .then(({ _id }) => {
+      console.log(_id);
       return User.findOneAndUpdate(
         { _id: params.userId },
         { $push: { thoughts: _id } },
